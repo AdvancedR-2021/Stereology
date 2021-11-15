ce88d65be8cea1adb3e1d4af882b903c40e037d2
Nikoleta Kakoulidou <kakoulidou.nikoleta@gmail.com>
2021-11-15 23:45:38 +0100

    fixed mispelled function name and arguments.

--- man/th_examples.R
+++ man/th_examples.R
@@ -1,13 +1,14 @@
-# loading the image.
+# Loading the image.
 
-# to use a PNG/JPEG/BMP image stored on your computer try:
+# To use a PNG/JPEG/BMP image stored on your computer try:
 # path <- "~/images.png"
-path <- system.file('extdata/sponge3.jpg',package='Sterology')
+path <- system.file('extdata/sponge3.jpg',package='Stereology')
 
-# applying mat.gen().
+# Applying mat.gen().
 mat <- mat.gen(path)
-mat
 
-# applying thr.i.ests()
-est.list <- thr.i.ests(m, 80, 145, 5, 1000)
+# Applying thr.i.ests()
+est.list <- th.i.ests(mtr = mat, x = 10, y = 10, d= 10, trial_s = 1000)
 est.list
+
+# For more details use: browseVignettes(package = "Stereology").
