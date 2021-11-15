2e643e13a6a59e0b28aa5b0f212fc058fccf2fcb
Nikoleta Kakoulidou <kakoulidou.nikoleta@gmail.com>
2021-11-15 23:05:55 +0100

    updated th examples.

--- man/th_examples.R
+++ man/th_examples.R
@@ -1,8 +1,11 @@
 # loading the image.
-path <- "D:/BACK-UP/msc - statistics/Advanced R programming/images.png"
+
+# to use a PNG/JPEG/BMP image stored on your computer try:
+# path <- "~/images.png"
+path <- system.file('extdata/sponge3.jpg',package='Sterology')
 
 # applying mat.gen().
-mat <- mat.gen()
+mat <- mat.gen(path)
 mat
 
 # applying thr.i.ests()
