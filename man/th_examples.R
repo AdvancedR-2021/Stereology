3213d01ca504e6d295e537819ef7292ffa0d0fcb
Nikoleta Kakoulidou <kakoulidou.nikoleta@gmail.com>
2021-11-18 14:15:19 +0100

    adjusted examples

--- man/th_examples.R
+++ man/th_examples.R
@@ -4,11 +4,11 @@
 # path <- "~/images.png"
 path <- system.file('extdata/sponge3.jpg',package='Stereology')
 
-# Applying mat_gen().
-mat <- mat_gen(path)
+# Applying bw_mat().
+a <- bw_mat(path, .5)
 
 # Applying th_i_ests()
-est_list <- th_i_ests(mtr = mat, x = 10, y = 10, d= 10, trial_s = 1000)
-est_list
+est_l <- th_i_ests(mtr = a$matrix, x = 400, y = 100, dx = 10, dy = 10,  trial_s = 1000)
+est_l
 
 # For more details use: browseVignettes(package = "Stereology").
